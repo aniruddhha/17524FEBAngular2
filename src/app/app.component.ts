0d0bd93d818779e3793add7ebef74603af994af6
aniruddha <aniruddha.kudalkar@gmail.com>
2017-02-11 14:47:44 +0530

    view encapsuslation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  encapsulation : ViewEncapsulation.Native
+  encapsulation : ViewEncapsulation.Emulated
 })
 export class AppComponent {
   title = 'app works!';
