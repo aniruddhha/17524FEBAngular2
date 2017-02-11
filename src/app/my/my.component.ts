6670668805b001c401864484181cd75ade2df553
aniruddha <aniruddha.kudalkar@gmail.com>
2017-02-11 14:30:39 +0530

    inline template and inline style

--- src/app/my/my.component.ts
+++ src/app/my/my.component.ts
@@ -2,8 +2,18 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-my',
-  templateUrl: './my.component.html',
-  styleUrls: ['./my.component.css']
+  // templateUrl: './my.component.html',
+  // styleUrls: ['./my.component.css']
+  template : `
+      <h1> Inline Template </h1>
+  `,
+  styles : [
+    `
+      h1 {
+        border : 1px solid red;
+      }
+    `
+  ]
 })
 export class MyComponent implements OnInit {
 
