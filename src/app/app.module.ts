70484811cf954e46efd89baafbe39d9982dc9a51
aniruddha <aniruddha.kudalkar@gmail.com>
2017-02-12 14:39:51 +0530

    input binding with some variable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { LoginComponent } from './login/login.component';
 import { ThinComponent } from './login/thin.component';
 import { DatabindingComponent } from './databinding/databinding.component';
 import { PropertyBindingComponent } from './databinding/property-binding.component';
+import { BlenderComponent } from './databinding/blender.component';
+import { EventBindingComponent } from './databinding/event-binding.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,9 @@ import { PropertyBindingComponent } from './databinding/property-binding.compone
     LoginComponent,
     ThinComponent,
     DatabindingComponent,
-    PropertyBindingComponent
+    PropertyBindingComponent,
+    BlenderComponent,
+    EventBindingComponent
   ],
   imports: [
     BrowserModule,
